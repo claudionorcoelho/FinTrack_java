9467c96e5fd0a8e69f87b227d51ef605790b26eb
Claudionor Coelho <clcoelho39@gmail.com>
2026-05-26 14:32:50 -0300

    Add MainController class

---
+++ src/main/java/com/fintrack/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.fintrack.controller;
+
+import javafx.fxml.FXML;
+
+/**
+ * Main controller for the application
+ */
+public class MainController {
+    
+    @FXML
+    public void initialize() {
+        // Initialize controller
+    }
+}
