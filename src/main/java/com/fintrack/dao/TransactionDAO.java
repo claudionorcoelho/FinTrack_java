59d9f30c204ec1466cfca06f2c67df3833053655
Claudionor Coelho <clcoelho39@gmail.com>
2026-05-29 11:06:44 -0300

    Adicionar comentários em português ao TransactionDAO

--- src/main/java/com/fintrack/dao/TransactionDAO.java
+++ src/main/java/com/fintrack/dao/TransactionDAO.java
@@ -4,12 +4,38 @@ import com.fintrack.model.Transaction;
 import java.util.List;
 
 /**
- * Data Access Object interface for Transaction
+ * Interface de Acesso a Dados (DAO) para Transação
+ * Define os métodos para operações de banco de dados relacionadas a transações
  */
 public interface TransactionDAO {
+    /**
+     * Insere uma nova transação no banco de dados
+     * @param transaction Objeto da transação a ser inserida
+     */
     void insert(Transaction transaction);
+    
+    /**
+     * Atualiza uma transação existente no banco de dados
+     * @param transaction Objeto da transação com dados atualizados
+     */
     void update(Transaction transaction);
+    
+    /**
+     * Deleta uma transação do banco de dados pelo ID
+     * @param id Identificador único da transação a ser deletada
+     */
     void delete(int id);
+    
+    /**
+     * Recupera uma transação específica do banco de dados pelo ID
+     * @param id Identificador único da transação
+     * @return Objeto Transaction encontrado, ou null se não existir
+     */
     Transaction getById(int id);
+    
+    /**
+     * Recupera todas as transações do banco de dados
+     * @return Lista contendo todas as transações armazenadas
+     */
     List<Transaction> getAll();
 }
